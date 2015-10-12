d1c6388f7a790a14364f7f200e3e92470d1df5f5
Jesse Soldat <jsoldat@hotmail.com>
2015-10-12 15:08:52 -0400

    added the live data link

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,23 @@
 
 (function () {
 
-  console.log('It Works!');
-})();
+  var etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop';
+
+  $.ajax({
+    url: etsyURL,
+    dataType: 'jsonp',
+    method: 'get'
+  }).then(function (response) {
+
+    //original code
+    var templateString = $('#itemTemplate').text();
+    var templateFunction = _.template(templateString);
+
+    _.each(response.results, function (item) {
+
+      var itemHTML = templateFunction(item);
+
+      $('#mainContent').append(itemHTML);
+    }); //end of each loop
+  }); // end of ajax call
+})(); // end of IIFE
